c23f7ea1338a996525d67f669264740fe10153aa
Oleg Zaytsev <ZaytsevOI.90@gmail.com>
2023-07-13 15:39:09 +1200

    feat: added component Error Boundary (#6)

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import { App } from './app';
 import { ThemeProvider } from './app/providers/ThemeProvider/ui/ThemeProvider';
 import 'shared/config/i18n';
+
 
 const root = document.getElementById('root') as HTMLElement;
 
 createRoot(root).render(
   <BrowserRouter>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <ErrorBoundary>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </ErrorBoundary>
   </BrowserRouter>,
 );
